5ea7710a3d1f82a18a9380e4963044e3891cf607
pay <opera.pay@gmail.com>
2019-05-21 02:53:57 +0700

    test deploy graph

--- js/button.js
+++ js/button.js
@@ -3,10 +3,18 @@ function myFunction () {
     var b = document.getElementById("pie");
     var c = document.getElementById("stack");
     var d = document.getElementById("scatter");
+    var e = document.getElementById("text-line");
+    var f = document.getElementById("text-pie");
+    var g = document.getElementById("text-stack");
+    var h = document.getElementById("text-scatter");
     c.style.display = "block";
     b.style.display = "none";
     d.style.display = "none";
     a.style.display = "none";
+    g.style.display = "block";
+    e.style.display = "none";
+    f.style.display = "none";
+    h.style.display = "none";
 };
 
 function myFunction1 () {
@@ -14,10 +22,18 @@ function myFunction1 () {
     var b = document.getElementById("pie");
     var c = document.getElementById("stack");
     var d = document.getElementById("scatter");
+    var e = document.getElementById("text-line");
+    var f = document.getElementById("text-pie");
+    var g = document.getElementById("text-stack");
+    var h = document.getElementById("text-scatter");
     b.style.display = "block";
     a.style.display = "none";
     d.style.display = "none";
     c.style.display = "none";
+    f.style.display = "block";
+    e.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
 };
 
 function myFunction2 () {
@@ -25,10 +41,18 @@ function myFunction2 () {
     var b = document.getElementById("pie");
     var c = document.getElementById("stack");
     var d = document.getElementById("scatter");
+    var e = document.getElementById("text-line");
+    var f = document.getElementById("text-pie");
+    var g = document.getElementById("text-stack");
+    var h = document.getElementById("text-scatter");
     d.style.display = "block";
     b.style.display = "none";
     a.style.display = "none";
     c.style.display = "none";
+    h.style.display = "block";
+    e.style.display = "none";
+    f.style.display = "none";
+    g.style.display = "none";
 };
 
 function myFunction3 () {
@@ -36,9 +60,17 @@ function myFunction3 () {
     var b = document.getElementById("pie");
     var c = document.getElementById("stack");
     var d = document.getElementById("scatter");
+    var e = document.getElementById("text-line");
+    var f = document.getElementById("text-pie");
+    var g = document.getElementById("text-stack");
+    var h = document.getElementById("text-scatter");
     a.style.display = "block";
     b.style.display = "none";
     d.style.display = "none";
     c.style.display = "none";
+    e.style.display = "block";
+    g.style.display = "none";
+    f.style.display = "none";
+    h.style.display = "none";
 };
 
